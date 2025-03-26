c361d15f9f7ad5263cd59775ddfd7bd5f65f6c97
rumforweb <ebertzz90@gmail.com>
2025-03-26 15:00:38 +0300

    change scss in BP

--- src/components/main/BestPractices.jsx
+++ src/components/main/BestPractices.jsx
@@ -1,6 +1,7 @@
 import { MENU_ITEMS } from '@/config/menuConfig'
 import { useModal } from '@/hooks/useModal'
 import DocModal from '@components/modal/DocModal'
+import { useRef } from 'react'
 
 import exlint from '@assets/img/exlint.svg'
 import CopyBtn from '@ui/CopyBtn/CopyBtn'
@@ -9,24 +10,34 @@ import styles from './BestPractices.module.scss'
 const BestPractices = () => {
 
 	const { isModalOpen, openModal, closeModal } = useModal();
+	const inputRef = useRef(null);
+	const installCommand = "npm install --global @exlint.io/cli"
+
+	 // Выделяем текст при клике на input
+	const handleInputClick = () => {
+		inputRef.current.select();
+	}
 
 	const heroContent = {
 		title: ["Empower efficient", "conventional best practices"],
 		description: 'Run your first complance check in a matter of minutes',
+		getStarted: "Get started for free today."
 	}
+
+	const INSTALL_COMMAND_ID = "exlint-install-command"; //чтобы избежать "магических строк"
 
 	return ( 
 			<section className={styles.empower}>
 				<div className={styles.wrapper}>
 					<div className={styles.window}>
 						<div className={styles.container}>
-
+							
 							<div className={styles.practice}>
 
-							<div className={styles.title}> 
-								{heroContent.title[0]}<br />{heroContent.title[1]}
-							</div>
-							<div className={styles.description}>{heroContent.description}</div>
+								<div className={styles.title}> 
+									{heroContent.title[0]}<br />{heroContent.title[1]}
+								</div>
+								<div className={styles.description}>{heroContent.description}</div>
 					
 								<ul className={styles.list}>
 
@@ -39,34 +50,38 @@ const BestPractices = () => {
 												Docs
 										</button>
 									
-										<a {...MENU_ITEMS.discord} className={styles.server}>Discord Server</a>
+										<a {...MENU_ITEMS.discord} className={styles.discord}>Discord Server</a>
 								
 								</ul>
-
 							</div>
 
-							<img className={styles.img} src={exlint} alt="Логотип Exlint"></img>
-
+							<img className={styles.exlintLogo} src={exlint} alt="Логотип Exlint"/>
 						</div>
 
-						<div className={styles.start}>
-							<div className={styles.today}>Get started for free today.</div>
-	
-								<code className={styles['console-wrapper']}>
+					
+						<div className={styles.today}>{heroContent.getStarted}</div>
+`						<div className={styles.consoleLine}>
+							<input 
+								ref={inputRef}
+								id={INSTALL_COMMAND_ID } // Уникальный ID для CopyBtn
+								aria-label="Installation command"
+								type="text"
+								value={installCommand}
+								readOnly
+								className={styles.installInput}
+								onClick={handleInputClick}
+								/>
 
-									<input id="install" type="text" value="npm install --global @exlint.io/cli" placeholder="npm install --global @exlint.io/cli"></input>
+							<CopyBtn targetId={INSTALL_COMMAND_ID} />
+				
+						</div>`
 
-									<CopyBtn targetId="install"/>
-
-								</code>
-
-								<button 
-									onClick={openModal}
-									className={styles[MENU_ITEMS.documentation.className]}
-									>
-										{MENU_ITEMS.documentation.label} {'>'}
-								</button>
-						</div>
+						<button 
+							onClick={openModal}
+							className={styles[MENU_ITEMS.documentation.className]}
+							>
+								{MENU_ITEMS.documentation.label} {'>'}
+						</button>
 					</div>
 				</div>
 					<DocModal isOpen={isModalOpen} onClose={closeModal} />
